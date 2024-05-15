5ed5ae4899a619e4298ac2c037e7456bdb50a545
Rahul Babu K <rahulbabuk05@gmail.com>
2024-05-15 11:46:52 +0530

    New Updates for Little Lemon Table Resevation Website

---
+++ CAPSTONE PROJECT/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src="/images/logo1.png" alt="Little Lemon Logo" />
+        </div>
+        <div className="footer-text">
+          <p>Copyright Little Lemon</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
