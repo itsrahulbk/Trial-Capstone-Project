b45b67bb13a77e710236f42279d632ef820c667e
Rahul Babu K <rahulbabuk05@gmail.com>
2024-05-15 11:37:44 +0530

    New Updates for Little Lemon Table Resevation Website About Page

--- CAPSTONE PROJECT/src/components/About.js
+++ CAPSTONE PROJECT/src/components/About.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import Project from './Project';
-import './Projects.css';
+import './About.css';
 
-const projectsData = [
-  {
-    title: "Project One",
-    description: "Description for project one.",
-    imageUrl: "%PUBLIC_URL%/images/project-one.png" // Replace with your image path
-  },
-  {
-    title: "Project Two",
-    description: "Description for project two.",
-    imageUrl: "%PUBLIC_URL%/images/project-two.png" // Replace with your image path
-  },
-  // Add more projects as needed
-];
-
-const Projects = () => {
+const About = () => {
   return (
-    <section id="projects" className="Projects">
-      <h2>Projects</h2>
-      <div className="Projects-list">
-        {projectsData.map((project, index) => (
-          <Project key={index} project={project} />
-        ))}
-      </div>
+    <section className="about" id="about">
+      <h2>About Little Lemon</h2>
+      <p>Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day.</p>
+      <p>Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy. Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region.</p>
     </section>
   );
 };
 
-export default Projects;
+export default About;
