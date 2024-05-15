8d90f36ccd086cb30dcf2ebb1aebda5b0f0b3c48
Rahul Babu K <rahulbabuk05@gmail.com>
2024-05-15 11:36:32 +0530

    New Updates for Little Lemon Table Resevation Website

---
+++ CAPSTONE PROJECT/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <h2>30% Off This Weekend</h2>
+        <p>Enjoy a delightful dining experience at Little Lemon with an exclusive 30% discount this weekend. Indulge in our mouth-watering dishes, made with the freshest ingredients and crafted to perfection. Don't miss out on this limited-time offer!</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
